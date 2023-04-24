386a01c07b2ae42c5ddb658ed432c734b46f9d33
agusdev <agusmartinezy30@gmail.com>
2023-04-23 23:39:57 -0300

    start app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+//logica
+<script setup>
+  const name = "mundo"
+  const styleColor = "color: yellow"
+</script>
+
+<template>
+  <h1>Hola {{ name }}</h1>
+  <h2 :style="styleColor">SOY AZUL</h2>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
